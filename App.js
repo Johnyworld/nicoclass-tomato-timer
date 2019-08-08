d5d288c5e61a6694a852f6090d3a8be1238815c5
Johny Kim <johnyworld@naver.com>
2019-08-08 21:14:34 +0900

    Initial Commit

---
+++ App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Timer from './components/Timer';
+import reducer from './reducer';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+let store = createStore(reducer);
+console.log(store.getState())
+
+export default class App extends Component{
+  render() {
+    return (
+      <Provider store={store}>
+        <Timer />
+      </Provider>
+    )
+  }
+} 
